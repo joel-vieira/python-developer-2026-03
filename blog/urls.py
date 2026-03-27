f9e49f7a2d6c42037cb68f4b5eb15a79832e28de
Joel Vieira <joelvieira94@gmail.com>
2026-03-27 00:57:24 -0400

    [FEATURE] Add posts index view

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("posts/", views.index, name="index"),
+    path("posts/", views.IndexView.as_view(), name="index"),
     path("", views.welcome, name="welcome"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,18 @@
+from django.db.models.query import QuerySet
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views import generic
+
+from .models import Post
 
 
-def index(request: HttpRequest) -> HttpResponse:
-    return render(request, "blog/index.html")
+class IndexView(generic.ListView):
+    template_name = "blog/index.html"
+    context_object_name = "published_posts"
 
+    def get_queryset(self) -> QuerySet:
+        return Post.objects.order_by("-published_at")
 
 def welcome(request: HttpRequest) -> HttpResponse:
+
     return render(request, "blog/welcome.html")
